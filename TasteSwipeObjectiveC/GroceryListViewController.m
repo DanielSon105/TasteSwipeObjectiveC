5d19f079f48db3208462cbb581d0e30149b6d948
DanielSon105 <daniel.g.barrido@gmail.com>
2015-11-19 14:30:52 -0600

    Grocery List Add feature implemented

--- TasteSwipeObjectiveC/GroceryListViewController.m
+++ TasteSwipeObjectiveC/GroceryListViewController.m
@@ -10,11 +10,17 @@
 #import "NetworkClient.h"
 #import "GroceryListItemTableViewCell.h"
 
-@interface GroceryListViewController () <UITabBarDelegate, UITableViewDataSource>
+@interface GroceryListViewController () <UITabBarDelegate, UITableViewDataSource, UITextFieldDelegate>
 @property NetworkClient *networkClient;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property NSMutableArray *groceryListItems;
 
+@property (weak, nonatomic) IBOutlet UIButton *addOrEditButton;
+@property (weak, nonatomic) IBOutlet UITextField *ingredientTextField;
+@property (weak, nonatomic) IBOutlet UITextField *unitOfMeasurementTextField;
+@property (weak, nonatomic) IBOutlet UITextField *amountTextField;
+
+@property GroceryListItem *selectedGroceryListItem;
 
 
 @end
@@ -23,6 +29,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.ingredientTextField.delegate=self;
+    self.unitOfMeasurementTextField.delegate=self;
+    self.amountTextField.delegate=self;
     self.networkClient = [NetworkClient new];
     self.groceries = [self.networkClient loadGroceryListFromCache];
     self.groceryListItems = [NSMutableArray new];
@@ -30,6 +39,7 @@
         GroceryListItem *item = [[GroceryListItem alloc] initWithJSON:dict];
         [self.groceryListItems addObject:item];
     }
+    self.selectedGroceryListItem = [GroceryListItem new];
 //    [self load];
     NSLog(@"Groceries --> %@",self.groceries);
     NSLog(@"Grocery List Items ---> %@", self.groceryListItems);
@@ -80,4 +90,73 @@
 
 }
 
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    self.selectedGroceryListItem = [self.groceryListItems objectAtIndex:indexPath.row];
+    self.ingredientTextField.text = self.selectedGroceryListItem.ingredient;
+    self.amountTextField.text = self.selectedGroceryListItem.amount;
+    self.unitOfMeasurementTextField.text = self.selectedGroceryListItem.unitOfMeasurement;
+}
+- (IBAction)onAddEditButtonTapped:(id)sender {
+    GroceryListItem *newItem = [GroceryListItem new];
+    newItem.amount = self.amountTextField.text;
+    newItem.ingredient = self.ingredientTextField.text;
+    newItem.unitOfMeasurement = self.amountTextField.text;
+
+    [self.groceryListItems addObject:newItem];
+    [self.tableView reloadData];
+    //SAVE PLIST
+}
+
+
+
+
+
+
+//    if ([self.addOrEditButton.titleLabel.text isEqualToString:@"Update"]) {
+//        NSDictionary *dict = [[NSDictionary alloc] initWithObjectsAndKeys:self.ingredientTextField.text, @"ingredient", self.amountTextField.text, @"amount", self.unitOfMeasurementTextField.text, @"unit", nil];
+//        GroceryListItem *newItem = [[GroceryListItem alloc] initWithJSON:dict];
+////        [self loadData];
+//        NSMutableArray *updatedGroceries = [self.groceryListItems copy];
+//        [updatedGroceries removeObjectAtIndex:self.tableView.indexPathForSelectedRow.row];
+//        [updatedGroceries addObject:newItem];
+//        self.groceryListItems = updatedGroceries;
+//        [self.tableView reloadData];
+//
+//
+////        [self.networkClient saveGroceryListToCache:updatedGroceries];
+////        NSMutableArray *tmpArray = [NSMutableArray new];
+////        for (NSDictionary *dict in updatedGroceries) {
+////            GroceryListItem *item = [[GroceryListItem alloc] initWithJSON:dict];
+////            [tmpArray addObject:item];
+////        }
+////        self.groceryListItems = tmpArray;
+////        [self.tableView reloadData];
+//    }
+//}
+
+#pragma mark - Keyboard Methods
+
+-(void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    [self animateTextField:textField up:YES];
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    [self animateTextField:textField up:NO];
+}
+
+-(void)animateTextField:(UITextField*)textField up:(BOOL)up
+{
+    const int movementDistance = -250; // tweak as needed
+    const float movementDuration = 0.3f; // tweak as needed
+
+    int movement = (up ? movementDistance : -movementDistance);
+
+    [UIView beginAnimations: @"animateTextField" context: nil];
+    [UIView setAnimationBeginsFromCurrentState: YES];
+    [UIView setAnimationDuration: movementDuration];
+    self.view.frame = CGRectOffset(self.view.frame, 0, movement);
+    [UIView commitAnimations];
+}
 @end
